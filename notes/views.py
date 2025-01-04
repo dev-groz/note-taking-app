41a3c50132b578df8473f22b19b2e0310927375e
dev-groz <groza4ka@gmail.com>
2025-01-04 13:42:33 +0700

    Add POST request support

--- notes/views.py
+++ notes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from notes.models import Note
 
@@ -10,3 +10,10 @@ def all_notes(req):
 def get_note(req, note_id):
     note = Note.objects.get(id=note_id)
     return render(req, 'singlenote.html', {'note': note})
+
+def create_note(req):
+    if req.method == 'POST':
+        header = req.POST.get('note-header')
+        text = req.POST.get('note-text')
+        return HttpResponse(header + " " + text)
+    return redirect('/')
